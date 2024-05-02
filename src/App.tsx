d2ffe6dd875eab1259666e0246adf34bf025fcab
MyaPwintEliza <104987808+MyaPwintEliza@users.noreply.github.com>
2024-05-02 15:54:53 +0630

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
   const [cardNumber, setCardNumber] = useState("");
   const [date, setDate] = useState("");
   const [cvc, setCvc] = useState("");
+  const [confirm, setConfirm] = useState("");
   return (
     <section>
       <div className="absolute -z-10">
@@ -103,6 +104,7 @@ export default function App() {
 
               <button
                 onClick={() => setConfrimed(true)}
+                value = {confrim}
                 type="submit"
                 className=""
               >
