e46f89d6c6b28eaaf984a74107e2b90420b21e8e
MyaPwintEliza <104987808+MyaPwintEliza@users.noreply.github.com>
2024-05-05 15:37:24 +0630

    Update ThankYouPage.tsx

--- src/compotents/ThankYouPage.tsx
+++ src/compotents/ThankYouPage.tsx
@@ -1,6 +1,17 @@
+import App from "../App";
 import Complete from "../images/icon-complete.svg";
+import React from "react";
 
-function ThankYouPage() {
+interface ThankYouPageProps {
+  setConfirmed: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ThankYouPage: React.FC<ThankYouPageProps> = ({ setConfirmed }) => {
+  React.useEffect(() => {
+    setConfirmed(true);
+
+    return () => setConfirmed(false);
+  }, [setConfirmed]);
   return (
     <div className="flex flex-col items-center justify-center h-screen mx-w-lg mx-auto">
       <img src={Complete} className="block mx-auto" />
@@ -10,9 +21,11 @@ function ThankYouPage() {
       <p className="text-slate-800 text-center">
         We've added your card details
       </p>
-      <button className="btn block mx-auto mt-10 w-full">Continue</button>
+      <button onChange={App} className="btn block mx-auto mt-10 w-full">
+        Continue
+      </button>
     </div>
   );
-}
+};
 
 export default ThankYouPage;
